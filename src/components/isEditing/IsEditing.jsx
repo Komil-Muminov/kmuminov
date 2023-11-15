30e07a34a136f38f7c6e13ec918642fb5cb31ad4
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2023-11-15 11:24:25 +0500

    fx

--- src/components/isEditing/IsEditing.jsx
+++ src/components/isEditing/IsEditing.jsx
@@ -1,20 +1,51 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { TextField, Button } from "@mui/material";
 const IsEditing = () => {
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm({ mode: "onBlur" });
+
+	const [userInput, setUserInput] = useState([{}]);
+	const handleOnSubmit = (data) => {
+		console.log(data);
+	};
 	return (
 		<>
 			<section className="sections isEditing-section bg-gray-50 rounded-xl mx-5 mb-5 p-14 ">
 				<div className="isEditing-container max-w-[1280px] m-auto">
-					<div className="form-wrapper flex flex-col items-center justify-center gap-5 p-5">
-						<TextField
-							className=" w-full md:w-1/4"
-							label="Задача "
-							type="search"
-							onChange={(e) => console.log(e.target.value)}
-						/>
-						<Button variant="outlined" className="md:p-5">
-							Добавить задачу
-						</Button>
+					<div className="form-wrapper ">
+						<form
+							onSubmit={handleSubmit(handleOnSubmit)}
+							className="isEditing-form form-wrapper flex flex-col items-center justify-center gap-5 p-5"
+						>
+							<TextField
+								className=" w-full md:w-1/4"
+								label="Задача "
+								type="search"
+								{...register("todo", {
+									required: "Заполните поле ",
+									minLength: {
+										value: 3,
+										message: "Необходимо внести минимум 3 символа",
+									},
+								})}
+							/>
+							<span className="form-error-message">
+								{errors?.todo?.message || ""}
+							</span>
+							<Button
+								onClick={handleSubmit(handleOnSubmit)}
+								variant="outlined"
+								className="md:p-5"
+							>
+								Добавить задачу
+							</Button>
+						</form>
 					</div>
+					<div className="items-wrapper">{}</div>
 				</div>
 			</section>
 		</>
